b64c15c822e509b343eb4d74b937322d77ae93e5
sihao <sihao@hujiang.com>
2018-01-27 18:14:27 +0800

    start rating

---
+++ js/demo01(1).js
@@ -0,0 +1,49 @@
+var rating = (function(){
+    //点亮星星
+    var lightOn = function ($item, num) {
+        $item.each(function (index) {
+            if (index < num) {
+                $(this).css('background-position', '0px -40px');
+            } else {
+                $(this).css('background-position', '0 0');
+            }
+        });
+    }
+
+
+    var init = function(el, num) {
+        var  $rating = $(el),
+             $item = $rating.find('.rating-item');
+
+
+        // 初始化
+        lightOn($item, num);
+
+
+        // 事件绑定
+        $rating.on('mouseover', '.rating-item', function () {
+            lightOn($item, $(this).index() + 1);
+        }).on('click', '.rating-item', function () {
+            num = $(this).index() + 1;
+        }).on('mouseout', function () {
+            lightOn($item, num);
+        })
+    }
+
+    // jQuery插件
+    $.fn.extand({
+        rating: function(n) {
+            return this.each(function() {
+                init(this, num);
+            })
+        }
+    })
+
+    return {
+        init: init
+    }
+})()
+
+rating.init('#rating', 2);
+// rating.init('#rating2', 3);
+$('#rating2').rating(4);
---
+++ js/demo01.js
@@ -0,0 +1,28 @@
+
+    var num = 2,
+        $rating = $('#rating'),
+        $item = $rating.find('.rating-item');
+    //点亮星星
+    var lightOn = function (num) {
+        $item.each(function (index) {
+            if (index < num) {
+                $(this).css('background-position', '0px -40px');
+            } else {
+                $(this).css('background-position', '0 0');
+            }
+        });
+    }
+    // 初始化
+    lightOn(num);
+
+
+    // 事件绑定
+    $item.on('mouseover', function () {
+        lightOn($(this).index() + 1);
+    }).on('click', function () {
+        num = $(this).index() + 1;
+    });
+
+    $rating.on('mouseout', function () {
+        lightOn(num);
+    })
